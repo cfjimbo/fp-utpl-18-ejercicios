8ef67a1d185a8b1f67f37131ccd4acf44b5460bd
cfjimbo <38107757+cfjimbo@users.noreply.github.com>
2018-05-17 01:01:13 -0500

    Repaso-ciclo-while

--- Repaso-ciclo-while/Ejercicio.java
+++ Repaso-ciclo-while/Ejercicio.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author Usuario
  */
-public class Ejercicio1 {
+public class Ejercicio {
 public static void main(String[] args) {
         // TODO code application logic here
         
